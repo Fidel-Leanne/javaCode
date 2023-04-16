60b5448af363037e31708a5d8fe0f4b66c9ac4bd
fidelia <fidelialtm13@gmail.com>
2023-04-16 02:00:03 +0200

    java2

---
+++ Programmingg.java/src/OOP/Main.java
@@ -0,0 +1,13 @@
+package src.OOP;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Jane myObj = new Jane();
+
+        myObj.myFavoriteColor();
+    }
+}
+
+    
+
---
+++ Programmingg.java/src/OOP/polymorphism.java
@@ -0,0 +1,19 @@
+package src.OOP;
+
+public class polymorphism{
+  public String color;
+public void myFavoriteColor(){
+
+System.out.println("My Favourite color is"+ color);
+ }
+    
+}
+
+class Jane extends polymorphism{
+ String  color ="red";
+    public void myFavoriteColor(){
+        System.out.println("My Favourite color is"+ color);
+
+    }
+}
+
